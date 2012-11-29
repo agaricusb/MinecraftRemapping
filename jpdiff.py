5c07070a376e56a3cc2aab4fe54d661b0c18ddc7
agaricus <agaricusb@yahoo.com>
2012-11-29 09:36:12 -0800

    Start of parsing javap -c output

--- jpdiff.py
+++ jpdiff.py
@@ -2,17 +2,54 @@
 
 # diff internally-renamed Java class files
 
-JAVAP = "javap -s -private -c"
+JAVAP = "javap -s -private -c "
+
 import os
 
-a = os.popen(JAVAP + " mc-dev/net/minecraft/server/ChunkSection").readlines()
-b = os.popen(JAVAP + " vanilla/zt").readlines()
+def disasm(fn):
+    f = os.popen(JAVAP + fn)
+    compiledFrom = f.readline().strip()
+    assert compiledFrom == "Compiled from SourceFile"
 
-assert len(a) == len(b), "Not equal length"
+    classDecl = f.readline().strip()
+    print classDecl
+   
+    while True:
+        memberDecl = f.readline()
+        if memberDecl == "": break
+        memberDecl = memberDecl.strip()
+        assert memberDecl[-1] == ";"
+        memberDecl = memberDecl.replace(";","")
 
-for i in range(len(a)):
-    if a[i] != b[i]:
-        print "-",a[i].strip()
-        print "+",b[i].strip()
-    else:
-        print " ",a[i].strip()
+        print memberDecl
+
+        sig = f.readline().strip()
+        assert sig.startswith("Signature: ")
+        sig = sig.replace("Signature: ", "")
+        print sig
+
+        line = f.readline().strip()
+        if line == "": continue
+        print line
+        assert line == "Code:"
+
+        while True:
+            codeLine = f.readline().strip()
+            if codeLine == "": break
+            print codeLine
+        print "---"
+
+disasm("mc-dev/net/minecraft/server/ChunkSection")
+
+def difflines():
+    a = os.popen(JAVAP + " mc-dev/net/minecraft/server/ChunkSection").readlines()
+    b = os.popen(JAVAP + " vanilla/zt").readlines()
+
+    assert len(a) == len(b), "Not equal length"
+
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            print "-",a[i].strip()
+            print "+",b[i].strip()
+        else:
+            print " ",a[i].strip()
