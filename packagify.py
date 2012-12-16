8efb21a890f23c2a8b898d4fc4bd6fc1df322269
Agaricus <agaricusb@yahoo.com>
2012-12-15 22:01:06 -0800

    Add script to translate raw class names to MCP package names

---
+++ packagify.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+# Prepend new MCP package names to classes from stdin to stdout
+
+import sys
+
+mcpdir = "../MinecraftForge/mcp/"
+
+def getPackages():
+    cls2pkg = {}
+    fn = mcpdir + "conf/packages.csv"
+    for line in file(fn).readlines():
+        cls, pkg = line.strip().split(",")
+        cls2pkg[cls] = pkg
+
+    return cls2pkg
+
+if __name__ == "__main__":
+    cls2pkg = getPackages()
+
+    while True:
+        line = sys.stdin.readline()
+        if len(line) == 0: break
+
+        inClass = line.strip()
+        if not cls2pkg.has_key(inClass):
+            print "No such class: "+inClass
+            continue
+
+        print cls2pkg[inClass] + "/" + inClass
+
+
