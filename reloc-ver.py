d7f10b5970d44fd2b9fb2d43bfbb300915493296
Agaricus <agaricusb@yahoo.com>
2012-12-21 18:06:51 -0800

    Fix generated srgs with added CB version relocation

--- reloc-ver.py
+++ reloc-ver.py
@@ -12,32 +12,20 @@ inVersion = sys.argv[1] # "v1_4_5"
 outVersion = sys.argv[2] # "v1_4_6"
 
 def addVersion(path, version):
-    if version == "-":
-        return path
+    if version == "nover": return path
 
-    components = path.split("/")
-
-    if path.startswith("net/minecraft/server/"):
-        #                <relocation>
-        #                    <pattern>net.minecraft.server</pattern>
-        #                    <shadedPattern>net.minecraft.server.v${minecraft_version}</shadedPattern>
-        #                </relocation>
-        #
-        components = components[:-1] + [version, components[-1]]
-    elif path.startswith("org/bouncycastle/"):
-        #                <relocation>
-        #                  <pattern>org.bouncycastle</pattern>
-        #                  <shadedPattern>net.minecraft.v${minecraft_version}.org.bouncycastle</shadedPattern>
-        #                </relocation>
-        components = ["net", "minecraft", version] + components
-
-    return "/".join(components)
-
-def addSigVersion(sig, version):
-    # TODO: parse more intelligently
-    sig = sig.replace("net/minecraft/server/", "net/minecraft/server/" + version + "/")
-    sig = sig.replace("org/bouncycastle/", "net/minecraft/" + version + "/org/bouncycastle/")
-    return sig
+    #                <relocation>
+    #                    <pattern>net.minecraft.server</pattern>
+    #                    <shadedPattern>net.minecraft.server.v${minecraft_version}</shadedPattern>
+    #                </relocation>
+    #
+    path = path.replace("net/minecraft/server/", "net/minecraft/server/" + version + "/")
+    #                <relocation>
+    #                  <pattern>org.bouncycastle</pattern>
+    #                  <shadedPattern>net.minecraft.v${minecraft_version}.org.bouncycastle</shadedPattern>
+    #                </relocation>
+    path = path.replace("org/bouncycastle/", "net/minecraft/" + version + "/org/bouncycastle/")
+    return path
 
 noverMode = inVersion == "nover"   # for making unversioned 1.4.5 -> versioned (with v1_4_5 package) - ignores output
 
@@ -63,9 +51,9 @@ for line in sys.stdin.readlines():
     elif kind == "MD:": # method
         inName, inSig, outName, outSig = args
         if noverMode:
-            print kind, inName, inVersion, inSig, addVersion(inName, outVersion), addSigVersion(outSig, outVersion)
+            print kind, inName, inSig, addVersion(inName, outVersion), addVersion(outSig, outVersion)
         else:
-            print kind, addVersion(inName, inVersion), inSig, addVersion(outName, outVersion), addSigVersion(outSig, outVersion)
+            print kind, addVersion(inName, inVersion), inSig, addVersion(outName, outVersion), addVersion(outSig, outVersion)
     else:
         print line
 
