545ce0a3a3605c996145fc1652d9d1dea5f8b04c
Agaricus <agaricusb@yahoo.com>
2012-12-22 14:05:31 -0800

    Add CB -> MCP method parameter name mappings (par1, par2..)

--- parameter-map.py
+++ parameter-map.py
@@ -19,17 +19,36 @@ def readCSV(path):
 
     return d
 
+# Read name remap from srg, full old name + sig -> new name + sig
+def readRemapSrg(path):
+    methodMap = {}
+    classMap = {}
+    for row in csv.reader(file(path), delimiter=" "):
+        if row[0] == "MD:":
+            oldName, oldSig, newName, newSig = row[1:]
+            methodMap[oldName + " " + oldSig] = (newName, newSig)
+        elif row[0] == "CL:":
+            oldName, newName = row[1:]
+            classMap[oldName] = newName
 
+    return methodMap, classMap
+    
 def main():
     if len(sys.argv) < 3:
-        print "Usage: %s mcp-conf-dir exc-file.exc" % (sys.argv[0],)
+        print "Usage: %s mcp-conf-dir exc-file.exc [method-name-remap.srg]" % (sys.argv[0],)
         print "Examples:"
-        print "\t%s ../MinecraftForge/mcp/conf packaged.exc" % (sys.argv[0],)
-        print "\t%s ../MinecraftForge/mcp/conf joined.exc" % (sys.argv[0],)
+        print "\t%s ../MinecraftForge/mcp/conf packaged.exc > 1.4.6/pkgmcp.methodparams" % (sys.argv[0],)
+        print "\t%s ../MinecraftForge/mcp/conf packaged.exc 1.4.6/pkgmcp2cb.srg > 1.4.6/cb2mcp.methodparams" % (sys.argv[0],)
+        print "\t%s ../MinecraftForge/mcp/conf joined.exc > 1.4.6/mcp.methodparams" % (sys.argv[0],)
         raise SystemExit
 
     mcpDir = sys.argv[1]
     excFilename = sys.argv[2]
+
+    if len(sys.argv) > 3:
+        methodNameRemap, classNameRemap = readRemapSrg(sys.argv[3])
+    else:
+        methodNameRemap = classNameRemap = None
 
     # Mapping from parameter number (p_####) to name in source (par#X..)
     paramNum2Name = readCSV(os.path.join(mcpDir, "params.csv"))
@@ -38,6 +57,8 @@ def main():
     methodNum2Name = readCSV(os.path.join(mcpDir, "methods.csv"))
 
     exc_re = re.compile(r"^([^.]+)\.([^(]+)(\([^=]+)=([^|]*)\|(.*)")
+    
+    ignoredClasses = set()
 
     for line in file(os.path.join(mcpDir, excFilename)).readlines():
         match = re.match(exc_re, line)
@@ -52,6 +73,35 @@ def main():
         else:
             # no one named this method
             methodName = methodNumber
+
+        # Second optional renaming pass, for translating MCP -> CB
+        if classNameRemap is not None and methodNameRemap is not None:
+            if classNameRemap.has_key(className):
+                newClassName = classNameRemap[className]
+            else:
+                # If remapping joined through CB, this is probably a client-only class - ignore it
+                #print "COULD NOT REMAP CLASS:" + className
+                newClassName = className
+                ignoredClasses.add(className)
+                continue
+            if className in ignoredClasses: continue
+
+            key = className + "/" + methodName + " " + methodSig
+            if methodNameRemap.has_key(key):
+                methodName, methodSig = methodNameRemap[key]
+            else:
+                if methodName == className.split("/")[-1]:
+                    # Method name is class name.. this is a constructor
+                    # No .srg remapping needed; since implied by class rename
+                    methodName = newClassName
+                else:
+                    # This is probably a client-only method - ignore it, too
+                    #print "COULD NOT REMAP METHOD:" + key
+                    continue
+
+
+            className = newClassName
+
 
         # List of classes thrown as exceptions
         exceptions = exceptionsString.split(",")
@@ -69,4 +119,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
+ 
