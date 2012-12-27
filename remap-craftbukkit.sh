b5176906d5f43de029c41c9fc73c831758fd918d
Agaricus <agaricusb@yahoo.com>
2012-12-26 20:56:52 -0800

    Stop astyle from creating .orig backups

--- remap-craftbukkit.sh
+++ remap-craftbukkit.sh
@@ -4,22 +4,25 @@
 CB_ROOT=../CraftBukkit
 
 # MCP decompiled with FML repackaging, but not joined. See https://gist.github.com/4366333
-MCP_PKGD_ROOT=~/minecraft/1.4.x/mcp725-pkgd
+MCP_PKGD_ROOT=../mcp725-pkgd
 
 # TODO: extract CB range map with Srg2Source, on slim
 # TODO: and MCP
 
 # TODO: apply prerenamefixes, minecraft-server-pkgmcp
 
-# TODO: run 
-#python rangeapply.py
+# TODO: parameters
+python rangeapply.py
 
 # TODO: apply javadoc
 
+# Reformat source style in NMS (only; not OBC) to more closely resemble MCP
 # This assumes you have astyle installed and ~/.astylerc copied from conf/astyle.cfg
-find $CB_ROOT/src -name '*.java' -exec astyle {} \;
+find $CB_ROOT/src/main/java/net/minecraft -name '*.java' -exec astyle --suffix=none {} \;
 
 # Measure differences to get a sense of progress
-diff -ur $MCP_PKGD_ROOT/src/minecraft_server/net/minecraft/ $CB_ROOT/src/main/java/net/minecraft/|wc -l
+diff -ur $MCP_PKGD_ROOT/src/minecraft_server/net/minecraft/ $CB_ROOT/src/main/java/net/minecraft/ > /tmp/diff
+
+wc -l /tmp/diff
 
 
