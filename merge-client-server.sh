32cd30709d776511065e0a504bf890927ff101b7
Agaricus <agaricusb@yahoo.com>
2013-01-03 20:12:13 -0800

    Try to merge CB patch into joined client/server. Failures because of new import statements.

---
+++ merge-client-server.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# MCP created using instructions on https://github.com/agaricusb/CraftBukkit/wiki/How-to-decompile-Minecraft-using-MCP-with-FML-repackaged-class-names,-without-FML's-other-changes
+# "with client/server joined"
+MCP_JOINED=../mcp726-cbjoined/
+
+# MCP/CB diff created using remap-craftbukkit.sh
+PATCH_FILE=/tmp/diff
+
+# Try to merge patch into joined client/server
+patch -d $MCP_JOINED/mcp/src_work/minecraft --dry-run -p4 < $PATCH_FILE
+# TODO: fix patch failures due to new import statements
+
