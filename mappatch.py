9e4c9633b2671bead9303bb01b08223480dcc510
Agaricus <agaricusb@yahoo.com>
2013-01-18 19:49:37 -0800

    Write remapped patch

--- mappatch.py
+++ mappatch.py
@@ -4,9 +4,12 @@ import subprocess, os
 
 srcRoot = "../CraftBukkit"
 scriptDir = "../jars"  # relative to srcRoot
+outDir = "../jars/cbpatches/" # relative to srcRoot
 startCommit = "d92dbbef5418f133f521097002c2ba9c9e145b8a"
-outDir = "cbpatches/"
 cbmcpBranch = "pkgmcp"
+
+shouldPullLatestChanges = False
+shouldRemapInitial = False
 
 def runRemap():
     print "Starting remap script..."
@@ -25,10 +28,7 @@ def runOutput(cmd):
     return subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read()
 
 
-def remap(commit):
-    run("git checkout "+commit)
-
-
+def buildRemappedPatch(commit):
     # Get the header from the original commit
     header = runOutput(("git", "show", commit, 
         "--format=email",       # email format is for git-format-diff/git-am
@@ -38,24 +38,58 @@ def remap(commit):
     # Compare against rename of previous commit
     cmd = ("git", "diff", "pkgmcp")
     diff = runOutput(cmd)
-    print diff
+
+    return header + "\n" + diff
+
+def clean():
+    # Clean out even non-repository or moved files
+    run("rm -rf src")
+    run("git reset --hard HEAD")
+
+def readCommitLog():
+    # Get commit IDs and messages after the starting commit, in reverse chronological order
+    commits = []
+    for line in runOutput(("git", "log", "--format=oneline")).split("\n"):
+        assert len(line) != 0, "Reached end of commit log without finding starting commit "+startCommit
+        commit, message = line.split(" ", 1)
+        if commit == startCommit: break
+        print commit, message
+        commits.append((commit, message))
+    commits.reverse()
+    return commits
 
 def main():
     if os.path.basename(os.getcwd()) != os.path.basename(srcRoot): os.chdir(srcRoot)
 
-    # Get all the latest changes
-    run("git checkout master")
-    run("git pull origin master")
+    if shouldPullLatestChanges:
+        # Get all the latest changes 
+        run("git checkout master")
+        run("git pull origin master")
 
-    # Start from here, creating an initial remap on a branch for the basis of comparison
-    run("git checkout "+startCommit)
-    run("git branch -D "+cbmcpBranch)
-    run("git checkout -b "+cbmcpBranch)
-    runRemap()
-    run("git commit -m 'Automated file rename'")  # separate commit for diff visibility
-    run("git commit -am 'Automated symbol rename'")
+    commits = readCommitLog()
 
-    # TODO: remap commits and compare
+    if shouldRemapInitial:
+        # Start from here, creating an initial remap on a branch for the basis of comparison
+        run("git checkout "+startCommit)
+        run("git branch -D "+cbmcpBranch)
+        run("git checkout -b "+cbmcpBranch)
+        runRemap()
+        run("git commit -m 'Automated file rename'")  # separate commit for diff visibility
+        run("git commit -am 'Automated symbol rename'")
+
+    # Remap commits
+    n = 0
+    for commitInfo in commits:
+        commit, message = commitInfo
+        n += 1
+        filename = "%s/%.4d-%s-%s" % (outDir, n, commit, message.replace(" ", "_"))
+        print "\n\n*** %s %s" % (commit, message)
+        clean()
+        run("git checkout "+commit)
+        runRemap()
+        
+        patch = buildRemappedPatch(commit)
+        file(filename, "w").write(patch)
 
 if __name__ == "__main__":
     main()
