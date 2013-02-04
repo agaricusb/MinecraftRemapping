d331466eaa41f89b3714cd31a70041cf2bfa6ef0
Agaricus <agaricusb@yahoo.com>
2013-02-03 20:56:55 -0800

    Ignore non-collisions

--- stringmap.py
+++ stringmap.py
@@ -14,6 +14,8 @@ if len(sys.argv) < 2:
 
 def main():
     packageMap, classMap, fieldMap, methodMap, methodSigMap = srglib.readSrg(sys.argv[1])
+    
+    oldNames = {}
 
     for oldFull, newFull in fieldMap.iteritems():
         oldName = srglib.splitBaseName(oldFull)
@@ -21,7 +23,25 @@ def main():
 
         if oldName == newName: continue  # skip un-renamed
 
-        print oldName,newName,oldFull,newFull
+        #print oldName,newName,oldFull,newFull
+
+        if not oldNames.has_key(oldName):
+            oldNames[oldName] = []
+        alreadyHave = False
+        for otherNewName, otherOldFull, otherNewFull in oldNames[oldName]:
+            if otherNewName == newName:
+                # different symbol luckily maps to same obf
+                # example: 2 log [[('a', 'net/minecraft/server/MinecraftServer/log', 'net/minecraft/server/MinecraftServer/a'), ('a', 'net/minecraft/server/WorldNBTStorage/log', 'ahv/a')]]
+                # ignore these since they aren't collisions
+                alreadyHave = True
+                break
+        if alreadyHave: continue
+
+        oldNames[oldName].append((newName,oldFull,newFull))
+
+    print
+    for oldName, newNames in oldNames.iteritems():
+        print len(newNames),oldName,[newNames]
 
     # TODO: method map
 
