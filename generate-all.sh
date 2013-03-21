fc939826a4edd44bf151770a961ab50c2fb66d2a
Agaricus <agaricusb@yahoo.com>
2013-03-20 20:37:05 -0700

    Add 1.5.1 prerelease mappings

--- generate-all.sh
+++ generate-all.sh
@@ -2,23 +2,22 @@
 
 # original obfuscated jar from Mojang
 # http://assets.minecraft.net/1_4_7/minecraft_server.jar
-VANILLA=minecraft_server-147.jar
-VANILLA_DIR=vanilla147
+VANILLA=minecraft_server151.jar
+VANILLA_DIR=vanilla151
 
 # internally renamed jar from Bukkit
 # http://repo.bukkit.org/content/repositories/releases/org/bukkit/minecraft-server/1.4.5/minecraft-server-1.4.5.jar
-MCDEV=minecraft-server-1.4.7.jar
-MCDEV_DIR=mcdev147
+MCDEV=minecraft-server-1.5.1.jar
+MCDEV_DIR=mcdev151
 
 # MCP deobfuscation mappings from Ocean Labs
-MCPCONF=../mcp726-clean/conf/
+MCPCONF=../mcp744pre2-clean/conf/
 
-# Repackaged FML/MCP - created as described on 
-# https://github.com/agaricusb/CraftBukkit/wiki/How-to-decompile-Minecraft-using-MCP-with-FML-repackaged-class-names,-without-FML's-other-changes
-PKGMCPCONF=../mcp726-pkgd/conf/
+# Repackaged FML/MCP
+PKGMCPCONF=../FML/mcp/conf/
 
 # Location to store generated mappings (must already exist)
-OUT_DIR=1.4.7/
+OUT_DIR=1.5.1/
 
 set -e
 
@@ -40,14 +39,14 @@ python chain.py $PKGMCPCONF $OUT_DIR/obf2cb.srg | sort > $OUT_DIR/cb2pkgmcp.srg
 
 
 # Reversed
-python reverse-srg.py $OUT_DIR/obf2cb.srg | sort > $OUT_DIR/cb2obf.srg
-python reverse-srg.py $OUT_DIR/cb2mcp.srg | sort > $OUT_DIR/mcp2cb.srg
-python reverse-srg.py $OUT_DIR/cb2pkgmcp.srg | sort > $OUT_DIR/pkgmcp2cb.srg
+python reverse-srg.py < $OUT_DIR/obf2cb.srg | sort > $OUT_DIR/cb2obf.srg
+python reverse-srg.py < $OUT_DIR/cb2mcp.srg | sort > $OUT_DIR/mcp2cb.srg
+python reverse-srg.py < $OUT_DIR/cb2pkgmcp.srg | sort > $OUT_DIR/pkgmcp2cb.srg
 
-# obf <-> MCP - this is like what MCP ships, except with merged descriptive symbol names
+# obf <-> MCP - this is like what MCP ships, except with merged descriptive "csv" symbol names (vs numeric "srg")
 python chain.py $OUT_DIR/cb2mcp.srg $OUT_DIR/cb2obf.srg | sort > $OUT_DIR/obf2mcp.srg # TODO: include client
 # special case chaining to '-' extracts complete obfuscated->mcp names without remapping
 python chain.py $PKGMCPCONF - | sort > $OUT_DIR/obf2pkgmcp.srg
-python reverse-srg.py $OUT_DIR/obf2mcp.srg | sort > $OUT_DIR/mcp2obf.srg
-python reverse-srg.py $OUT_DIR/obf2pkgmcp.srg | cat > $OUT_DIR/pkgmcp2obf.srg # TODO: sort
+python reverse-srg.py < $OUT_DIR/obf2mcp.srg | sort > $OUT_DIR/mcp2obf.srg
+python reverse-srg.py < $OUT_DIR/obf2pkgmcp.srg | sort > $OUT_DIR/pkgmcp2obf.srg
 
