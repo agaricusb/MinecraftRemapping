a0c159af2f9ed8841d807b0ef7fea249a7b22def
Agaricus <agaricusb@yahoo.com>
2013-04-09 19:44:16 -0700

    Add option for expanding remapped Spigot
    Created from https://github.com/EcoCityCraft/Spigot-Server by running https://github.com/MinecraftForge/Srg2Source:
    
    py remap-craftbukkit.py --cb-dir=../../CraftBukkit --fml-dir=fml --out-dir /tmp/out --skip-output-archive --skip-finish-cleanup --skip-compile
    
    ..until MCPBukkit can be setup automatically remap Spigot's patches.

--- expandmcpb.py
+++ expandmcpb.py
@@ -6,23 +6,31 @@
 
 import shutil, difflib, os, sys
 
+SPIGOT = len(sys.argv) > 1
+print "Spigot?",SPIGOT
+
 outDir = "/tmp/out"
 fmlDir = "../Srg2Source/python/fml"
 mcpDir = os.path.join(fmlDir, "mcp")
 vanillaSrc = os.path.join(mcpDir, "src/minecraft_server") # decompiled server with srgnames
 cbPatches = "../MCPBukkit/patches"
 obc = "../MCPBukkit/src"
+spigot = "../CraftBukkit/src/main/java" # Spigot-Server (patches applied)
 
 print "Copying vanilla source"
 if os.path.exists(outDir):
     shutil.rmtree(outDir)
-shutil.copytree(vanillaSrc, outDir)
-shutil.copytree(os.path.join(obc, "org"), os.path.join(outDir, "org"))
+if SPIGOT:
+    shutil.copytree("../CraftBukkit/src/main/java", outDir)
+else:
+    shutil.copytree(vanillaSrc, outDir)
+    shutil.copytree(os.path.join(obc, "org"), os.path.join(outDir, "org"))
 
-print "Applying CraftBukkit NMS patches"
-sys.path.append(os.path.join(fmlDir))
-import fml
-fml.apply_patches(mcpDir, cbPatches, os.path.join(outDir, "net"))  # .../
+if not SPIGOT:
+    print "Applying CraftBukkit NMS patches"
+    sys.path.append(os.path.join(fmlDir))
+    import fml
+    fml.apply_patches(mcpDir, cbPatches, os.path.join(outDir, "net"))  # .../
 
 sys.path.append(mcpDir)
 import runtime.commands
